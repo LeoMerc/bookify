299ac9519820e201ee3dbe6e4c029838c53ec879
Alejandro-Fierro <luis.fierro193@tectijuana.edu.mx>
2022-11-23 20:31:04 -0800

    valiendo verga

--- lib/providers/libro_provider.dart
+++ lib/providers/libro_provider.dart
@@ -3,7 +3,7 @@ import 'dart:convert';
 import 'package:bookify/classes/libro.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
+
 
 import '../const.dart';
 
